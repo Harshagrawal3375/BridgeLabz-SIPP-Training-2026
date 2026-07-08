fd3307c97bfc6ab158bbb984becab7e0f9dbd6fd
Harsh Agrawal <harshagrawal3375@gmail.com>
2026-07-08 12:25:01 +0530

    [Harsh] add : Binary Search

---
+++ Collections-in-DSA/Binary-Search/PeakElement.java
@@ -0,0 +1,17 @@
+
+class PeakElement{
+    public int findPeakElement(int[] arr) {
+        int left = 0, right = arr.length - 1;
+
+        while (left < right) {
+            int mid = left + (right - left) / 2;
+
+            if (arr[mid] < arr[mid + 1]) {
+                left = mid + 1; // go right
+            } else {
+                right = mid; // go left
+            }
+        }
+        return left; // peak index
+    }
+}
